f6ba8ee895a331da9289686031d91a98ab303e1e
Mayank malviya <mayankmalviya440@gmail.com>
2026-01-30 09:32:21 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
+import random
 
+print("🎲 Welcome to Dice Simulator developed by Monty")
+
+while True:
+    roll = input("Press Enter to roll the Dice or type 'exit' to quit")
+
+    if roll == "exit":
+        print("Thanks for playing")
+        break
+
+    dice_number = random.randint(1,6)
+    print("You Rolled:",dice_number)
